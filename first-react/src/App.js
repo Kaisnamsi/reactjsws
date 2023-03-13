e19fc2b51f23ec53fb081686f731051a5aa983d0
Kaisnamsi <kaisnamsi@gmail.com>
2023-03-13 16:27:54 +0100

    first commit

---
+++ first-react/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Sign Up</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" placeholder="Enter your password" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+      <div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name"/>
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"/>
+  </div>
+</div>
+</div>
+    
+  );
+  
+}
+
+export default App;
